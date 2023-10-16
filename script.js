c708ae01cd118863db425a4f3e8e82d613c95a29
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-10-15 23:08:01 -0400

    finish javascript added leapyear

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let output_day = document.querySelector(".days");
+let output_day = document.querySelector(".total_days");
 let output_month = document.querySelector(".months");
 let output_year = document.querySelector(".years");
 
@@ -9,28 +9,23 @@ const submit_button = document.querySelector(".submit");
 let current_day = new Date().getDate();
 let current_month = new Date().getMonth() + 1;
 let current_year = new Date().getFullYear();
-let years_difference = 0;
-let current_months_in_year = 0;
-let months_to_days = 0;
-let total_days = 0;
-let total_months = 0;
-
-console.log(current_day);
 
 submit_button.addEventListener("click", () => {
   let input_day = document.getElementById("day").value;
   let input_month = document.getElementById("month").value;
   let input_year = document.getElementById("year").value;
 
-  let sliced_days;
+  let years_difference = 0;
+  let current_months_in_year = 0;
+  let total_days = 0;
+  let leap_years = 0;
 
-  years_to_days = current_year * 365;
-  total_days -= input_day;
+  let sliced_days;
 
   //////TAKES BIRTH YEAR AND TURNS INTO DAYS THEN ADDS TO TOTAL_DAYS
   // 1. takes month person was born then takes that month and the rest of the months in that year //
   sliced_days = months.slice(input_month);
-  //2. converts it to days  //
+  //2. converts it to total_days  //
   sliced_days.forEach((day) => {
     total_days += day;
   });
@@ -38,22 +33,27 @@ submit_button.addEventListener("click", () => {
   total_days -= input_day;
 
   /////TAKES DIFFERENCE IN BIRTH AND CURRENT YEAR AND TURNS INTO DAYS THEN ADDS TO TOTAL_DAYS
-  //total amount of years between birthyear and current year
+  //1.total amount of years between birthyear and current year
   years_difference = current_year - input_year;
-  //subtract 1 for birth year//
+  //2.subtract 1 for birth year//
   --years_difference;
-  //multiply year difference bby 365 to get difference in days//
+  //3.multiply year difference bby 365 to get difference in total_days//
   total_days += years_difference * 365;
 
   ////TAKES CURRENT AMOUNT OF DAYS IN CURRENT YEAR AND ADDS TO TOTAL_DAYS
   //1. takes january to current month//
-  current_months_in_year = months.slice(0, current_month);
+  current_months_in_year = months.slice(0, current_month + 1);
+  //2.takes current months in years and adds em
+  current_months_in_year.forEach((day) => {
+    total_days += day;
+  });
+  total_days -= months[current_month] - current_day;
+
+  ////////ADDS LEAP YEARS///
+  //1.takes total of total_days and divides by 1460 (number of total_days in 4 years)
+  leap_years = Math.floor(total_days / 1460);
+  //2. adds leap years to total total_days
+  total_days += leap_years;
 
   console.log(total_days);
-  console.log(current_months_in_year);
-
-  //2.takes current months in years and adds em
-
-  //3. takes current month and subtracts remainder of days then subtracts from total days
-  total_days -= months[current_month] - current_day;
 });
